3ba20c164105213de887881abbc1840bf52ab514
PeskyByte <hashem.aljarrah.ha@gmail.com>
2024-07-19 02:21:38 +0300

    Test priority queue

--- PriorityQueue/src/Main.java
+++ PriorityQueue/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PriorityQueue<Integer> x = new PriorityQueue<>();
+        x.insert(3);
+        x.insert(1);
+        x.insert(2);
+        x.insert(3);
+        System.out.println(x.size());
+        System.out.println(x.pop());
+        System.out.println(x.pop());
+        System.out.println(x.pop());
+        System.out.println(x.pop());
     }
 }
