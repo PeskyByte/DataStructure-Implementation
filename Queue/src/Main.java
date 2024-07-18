a19f64658e8dbfface5bf2dd340d602aa8a24791
PeskyByte <hashem.aljarrah.ha@gmail.com>
2024-07-18 18:02:08 +0300

    Test queue implementation

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Queue<Integer> x = new Queue<Integer>();
+        x.enqueue(1);
+        x.enqueue(2);
+        x.enqueue(3);
+
+        for(Integer i: x){
+            System.out.println(i);
+        }
+        System.out.println("Size = " + x.size());
+
+        System.out.println(x.dequeue());
+        System.out.println(x.dequeue());
+        System.out.println(x.dequeue());
+
+        System.out.println("Size = " + x.size());
+        System.out.println("Is empty? " + x.isEmpty());
     }
 }
